ce5388fa8e32ae5d45e9021ddf93d6fbd52cc321
Stimur2304 <samatov.timur33@gmail.com>
2024-01-28 17:06:35 +0600

    constants

---
+++ src/pages/constants/partners.jsx
@@ -0,0 +1,12 @@
+import  partner1 from "../main/pictures/partner1.png"
+import partner2 from "../main/pictures/partner2.png"
+
+export const partners = [
+        {
+           img:`${partner1}`
+        },
+        {
+            img:`${partner2}`
+        }
+    ]
+
