5432e6e6f8de26d81491cfe5e66ef48f1535ebb3
Stimur2304 <samatov.timur33@gmail.com>
2024-01-28 17:45:25 +0600

    partners ended

--- src/pages/constants/partners.jsx
+++ src/pages/constants/partners.jsx
@@ -1,12 +1,25 @@
 import  partner1 from "../main/pictures/partner1.png"
 import partner2 from "../main/pictures/partner2.png"
+import partner3 from "../main/pictures/partner3.png"
+import partner4 from "../main/pictures/partner4.png"
+import partner5 from "../main/pictures/partner5.png"
 
-export const partners = [
+
+export const Partners = [
         {
            img:`${partner1}`
         },
         {
             img:`${partner2}`
+        },
+        {
+            img:`${partner3}`
+        },
+        {
+            img:`${partner4}`
+        },
+        {
+            img:`${partner5}`
         }
     ]
 
--- src/pages/main/partners/partner.jsx
+++ src/pages/main/partners/partner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./partner.css"
 import finalProjectReactangle from "../../main/pictures/finalProjectReactangle.png"
+import { Partners } from '../../constants/partners'
 
 const Partner = () => {
   return (
@@ -13,8 +14,19 @@ const Partner = () => {
                 <img src={finalProjectReactangle} alt="" />
             </div>
         </div>
+        <div>
+          <p className='partner__header'>Our Partners</p>
+        </div>
         <div className='partners'>
-          
+          {Partners.map((item,idx)=>{
+            return(
+              <div className='img-div__partners'>
+                <div key={idx}>
+                <img src={item.img} alt="" />
+                </div>
+              </div>
+            )
+          })}
         </div>
     </div>
   )
