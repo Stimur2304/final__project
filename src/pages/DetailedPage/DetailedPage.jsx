fb0fe051c35ec61303de491bfc740cf1c891b1f9
Daniel <iapyzov8@gmail.com>
2024-01-28 16:05:36 +0600

    tst

---
+++ src/pages/DetailedPage/DetailedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderPart from '../main/headerr/header'
+
+const DetailedPage = () => {
+  return (
+    <div>
+        <HeaderPart></HeaderPart>
+    </div>
+  )
+}
+
+export default DetailedPage
