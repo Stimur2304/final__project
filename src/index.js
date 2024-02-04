140c9b6a6d8031df923d7d97d1aab0ec9cdb1a07
Stimur2304 <samatov.timur33@gmail.com>
2024-02-04 18:54:07 +0600

    button-alert

--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
     <App/>
+        <ToastContainer
+
+        position="top-center"
+
+    autoClose={5000}
+
+    hideProgressBar={false}
+
+    newestOnTop={false}
+
+    closeOnClick
+
+    rtl={false}
+
+    pauseOnFocusLoss
+
+    draggable
+
+    pauseOnHover
+
+    theme="light"
+
+/>
     </BrowserRouter>
 );
 
