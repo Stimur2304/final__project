838d96748631f34a95c6e329008f116394f8df90
Daniel <iapyzov8@gmail.com>
2024-02-04 14:48:39 +0600

    Fotki

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+
 const axiosInstance = axios.create({ 
     baseURL:"https://realty-ggcv.onrender.com"
 })
--- src/pages/DetailedPage/DetailedPage.jsx
+++ src/pages/DetailedPage/DetailedPage.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderPart from '../main/headerr/header'
+import { useParams } from 'react-router-dom'
+import {axiosInstance} from '../../API/index'
+import ReactImages from '../../components/ReactImage/ReactImages'
 
 const DetailedPage = () => {
+
+  const {id} = useParams()
+  const [data, setData] = useState({})
+
+  const GetOneFlat = async () => {
+    try {
+        const response = await axiosInstance.get(`/main/flats/${id}`)
+        setData(response.data)
+    } catch (e) {
+
+    } finally {
+
+    }
+  }
+
+  useEffect(() => {
+    GetOneFlat()
+  }, [])
+
   return (
     <div>
-        <HeaderPart></HeaderPart>
+        <HeaderPart />
+        <ReactImages 
+          photos={data?.flat_images}
+        />
     </div>
   )
 }
