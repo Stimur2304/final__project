5c518069cedcd6c564b41394540d56fa9097472e
Stimur2304 <samatov.timur33@gmail.com>
2024-02-11 10:13:11 +0600

    final vesion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { React } from "react";
 import Homepage from "./pages/main/main";
 import DetailedPage from './pages/DetailedPage/DetailedPage';
 import LogInPage from './pages/LogIn/LogIn';
+import AboutUS from './pages/main/aboutUS/aboutUS';
 
 function App() {
   return(
@@ -12,6 +13,7 @@ function App() {
     <Route exact path='/' element={<Homepage/>}/>
     <Route exact path="/:id"   element={<DetailedPage/>}/>
     <Route exact path="/logIn"   element={<LogInPage/>}/>
+    <Route exact path="aboutUs" element={<AboutUS/>}/>
     </Routes>
   </div>
   )
